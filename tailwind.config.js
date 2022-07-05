4d6d7d5cf1cbd0b6fb9148fe3873850b7c16eea6
ttommyth <ttommyth@gmail.com>
2022-07-05 11:10:44 +0800

    INIT project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+  ],
+}
